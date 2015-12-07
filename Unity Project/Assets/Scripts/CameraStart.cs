107e19a76a40ac82acb694fde24ac744d3a61072
enryls <enryls@hotmail.it>
2015-12-07 22:17:23 +0100

    Sample Menu
    Try a sample menu with some script

---
+++ Unity Project/Assets/Scripts/CameraStart.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class CameraStart : MonoBehaviour {
+	
+	static public int posCamera;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKey && posCamera<1)
+				posCamera = 1;
+		PosizioneCamera ();
+	
+	}
+
+	void PosizioneCamera(){
+		switch (posCamera) {
+			case(1):
+				transform.position = new Vector3 (-1000f, 0f, -17f);
+			break;
+			case(2):
+                transform.position = new Vector3(0f, 0f, -17f);
+                break;
+			case(3):
+                transform.position = new Vector3(1000f, 0f, -17f);
+                break;
+			
+
+		}
+	}
+}
---
+++ Unity Project/Assets/Scripts/Menu.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+	public bool isMale = false;
+	public bool isFemale = false;
+	Color maincolor;
+	Color onMouseEntercolor;
+	Color onMouseClickColor;
+	// Use this for initialization
+	void Start () {
+		maincolor = new Color (0.9607f, 0.6784f, 0.3450f, 1f);
+		onMouseEntercolor = new Color (0.5764f, 0.3176f, 0.5686f, 1f);
+		onMouseClickColor = new Color (0.2f, 0.1882f, 0.1921f, 1f);
+        GetComponent<Renderer>().material.color = maincolor;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Escape)){
+			switch (CameraStart.posCamera) {
+			case(1):
+				Application.Quit();
+				break;
+			}
+
+		
+		}
+		/*if (Input.GetKey (KeyCode.Return)) {
+			Application.LoadLevel ("FirstLv");
+		}*/
+	}
+
+	void OnMouseEnter(){
+        GetComponent<Renderer>().material.color = onMouseEntercolor;
+	}
+
+	void OnMouseExit(){
+        GetComponent<Renderer>().material.color = maincolor;
+	}
+	void OnMouseUp(){
+        GetComponent<Renderer>().material.color = onMouseClickColor;
+		if (isFemale)
+			CameraStart.posCamera = 2;
+		if (isMale)
+            CameraStart.posCamera = 3;
+
+    }
+}
